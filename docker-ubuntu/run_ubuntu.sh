d407e256973cc2007c059fa23e014ff805543d3a
RockFc <769357879@qq.com>
2024-10-11 16:48:47 +0800

    update scripte

--- docker-ubuntu/run_ubuntu.sh
+++ docker-ubuntu/run_ubuntu.sh
@@ -1,4 +1,4 @@
 CONTAINER_NAME="my-ubuntu"
 IMAGE_NAME="my-ubuntu"
-TAG=23.04.$1
-docker run -d -i -t --net=host --name $CONTAINER_NAME-$TAG -v/Users/fuchao/Workspace:/workspace  $IMAGE_NAME:$TAG
+TAG=23.04.3
+docker run -d -i -t -p 10001-10100:10001-10100/tcp -p 10001-10100:10001-10100/udp -p 8080:8080/tcp  --name $CONTAINER_NAME -v/Users/fuchao/Workspace:/workspace  $IMAGE_NAME:$TAG
